b447e751db54b037fe86762a8deb8f8d45cc5b68
treva556 <marktreva92@gmail.com>
2023-08-20 01:45:29 +0300

    initial

---
+++ chess.cs
@@ -0,0 +1,7 @@
+void CreateGraphicalBoard () {
+    for (int file = 0; file < 8; file ++) {
+        for (int rank = 0; rank < 8; rank ++) {
+            bool isLIghtSquare = (file + rank)  % 2 != 0;
+        }
+    }
+}
